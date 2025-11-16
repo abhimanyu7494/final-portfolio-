9fc7a0bd6da087c1135ba98a9b42c81cfd470362
abhimanyu7494 <sabhimanyu9263@gmail.com>
2025-11-16 17:07:46 +0530

    Remove skills and experiences from About component
    Removed skills and experiences arrays from About component.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,19 +2,6 @@ import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 
 const About = () => {
-  const skills = [
-    { name: "HTML", icon: <FaHtml5 color="#E34F26" /> },
-    { name: "CSS", icon: <FaCss3Alt color="#1572B6" /> },
-    { name: "JavaScript", icon: <FaJs color="#F7DF1E" /> },
-    { name: "React", icon: <FaReact color="#61DAFB" /> },
-    { name: "Node.js", icon: <FaNodeJs color="#339933" /> },
-    { name: "Express", icon: <SiExpress color="#000000" /> },
-  ];
-
-  const experiences = [
-    "Currently working at Dataline Imagin Pvt. Ltd. as a Full Stack Developer",
-    "Completed 3 internships at: Internpe, ApexPlanet, PlaceLab",
-  ];
 
   return (
     <section id="about" className="container" style={{ padding: "80px 20px" }}>
